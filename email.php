93c7d7c7360af163e8ac80abe9e08ebb4dda5bc3
Michael Wenger <michaelwenger27@gmail.com>
2013-10-11 17:10:05 -0400

    Removed extraneous echo

--- email.php
+++ email.php
@@ -36,7 +36,6 @@
 		      {
 		      move_uploaded_file($_FILES["attachment"]["tmp_name"],"upload/" . $_FILES["attachment"]["name"]);
 		      $fullTmpPath = FILEPATH . "upload/" . $_FILES["attachment"]["name"];
-		      echo $fullTmpPath;
 		      return $fullTmpPath;
 		      }
 		    }
