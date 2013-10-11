4bfbbf9ac031d0b94cf7bd49028f05e121106361
Michael Wenger <michaelwenger27@gmail.com>
2013-10-11 17:28:39 -0400

    Updated config settings

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
-define('FILEPATH', '/home/mikewenger/mbwenger.com/digital_strategy_form/');
-define('PRIMARY_EMAIL', 'cases@michaeljfox.fogbugz.com');
+define('FILEPATH', '/home/mikewenger/mbwenger.com/test/');
+define('PRIMARY_EMAIL', 'michaelwenger27@gmail.com');
 ?>
