99e73319dcd5fc605db8d7a6573936002a1f89fc
Michael Wenger <michaelwenger27@gmail.com>
2013-10-11 17:03:44 -0400

    Fixing

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 
-define('FILEPATH', 'home/mikewenger/mbwenger.com/digital_strategy_form/');
+define('FILEPATH', '/home/mikewenger/mbwenger.com/digital_strategy_form/');
 define('PRIMARY_EMAIL', 'cases@michaeljfox.fogbugz.com');
 ?>
