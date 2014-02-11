43ec01129fff5078f7d5f9d617c24b5665989535
Michael Wenger <mwenger@michaeljfox.org>
2014-02-11 12:36:59 -0500

    Testing 2 cc options

--- index.php
+++ index.php
@@ -183,7 +183,7 @@ if(count($_POST) > 0){
 
 		case 'sendemail':
 			$subject = "EMAIL: " . $_POST["emailName"];
-			$to = array('to' => 'sbourque@michaeljfox.org', 'cc' => array('nryerson@michaeljfox.org', 'Nancy Ryerson'), 'bcc' => 'nmarino@michaeljfox.org');
+			$to = array('to' => 'sbourque@michaeljfox.org', 'cc' => array('nryerson@michaeljfox.org', 'mwenger@michaeljfox.org'), 'bcc' => 'nmarino@michaeljfox.org');
 			$from = $_POST["fromemail"];
 			$message .= "Team: " . $_POST["department"] . "<br><br>";
 			$message .= "Date: " . $_POST["emailDate"] . "<br><br>";
